6c1c1db770d95ba92f2d4bfeeb5ccd76d889321d
Akshay <Akshay@10.0.2.15>
2017-05-18 21:07:24 +0100

    Adding

--- ShiftService/routes/route.js
+++ ShiftService/routes/route.js
@@ -2,21 +2,13 @@ var express = require('express');
 var TempShifts = require('../models/shift');
 var router = express.Router();
 
-router.route('/shift/:date')
-
-.get(function(req,res){
-var shifts = [];
-for(var shiftList = 0; shiftList < TempShifts.length; shiftList++) {
-    var shift= TempShifts[shiftList];
-    if (shift.ShiftDate==req.params.date && shift.Assigned=="false")
-    {
-      shifts.push(shift);
-    }
-}
-  res.json(shifts);
-})
-
-
-
+app.get('/shifts', function (req, res){
+    TempShifts.find({}, 'shift', function(err, shifts){
+        if(err){
+        } else{
+            res.render('shift-list', shifts);
+        }
+    })
+});
 
 module.exports = router;
